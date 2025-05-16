e197e2f11bfc2f2c9fb3eefbb34f03250e3b344d
mehdi nasery pak <160924833+MehdiNaseryPak@users.noreply.github.com>
2025-05-16 09:54:28 +0330

    connection to db and make first route

---
+++ initializers/connectionDB.go
@@ -0,0 +1,26 @@
+package initializers
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectionDB() {
+	var err error
+	db_host := os.Getenv("DB_HOST")
+	db_port := os.Getenv("DB_PORT")
+	db_user := os.Getenv("DB_USER")
+	db_password := os.Getenv("DB_PASSWORD")
+	db_name := os.Getenv("DB_NAME")
+	dsn := db_user+":"+db_password+"@tcp("+db_host+":"+db_port+")/"+db_name+"?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("connection error")
+	}
+}
